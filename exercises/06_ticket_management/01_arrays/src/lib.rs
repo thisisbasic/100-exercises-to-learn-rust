6044775e93083026f8eab5d3a57d31259a8b52e3
ThisIsBasic <learningstill@proton.me>
2024-07-17 14:11:05 +0200

    ch 06 ex 02 solution

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
 pub struct WeekTemperatures {
-    // TODO
+    pub temperatures: [Option<i32>; 7],
 }
 
 pub enum Weekday {
@@ -16,15 +16,31 @@ pub enum Weekday {
 
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        Self {
+            temperatures: [None; 7],
+        }
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        let index = self.week2index(&day);
+        self.temperatures[index]
     }
 
-    pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+    pub fn set_temperature(&mut self, day: &Weekday, temperature: i32) {
+        let index = self.week2index(day);
+        self.temperatures[index] = Some(temperature);
+    }
+
+    pub fn week2index(&self, day: &Weekday) -> usize {
+        match day {
+            Weekday::Monday => 0,
+            Weekday::Tuesday => 1,
+            Weekday::Wednesday => 2,
+            Weekday::Thursday => 3,
+            Weekday::Friday => 4,
+            Weekday::Saturday => 5,
+            Weekday::Sunday => 6,
+        }
     }
 }
 
@@ -44,18 +60,18 @@ mod tests {
         assert_eq!(week_temperatures.get_temperature(Weekday::Saturday), None);
         assert_eq!(week_temperatures.get_temperature(Weekday::Sunday), None);
 
-        week_temperatures.set_temperature(Weekday::Monday, 20);
+        week_temperatures.set_temperature(&Weekday::Monday, 20);
         assert_eq!(week_temperatures.get_temperature(Weekday::Monday), Some(20));
 
-        week_temperatures.set_temperature(Weekday::Monday, 25);
+        week_temperatures.set_temperature(&Weekday::Monday, 25);
         assert_eq!(week_temperatures.get_temperature(Weekday::Monday), Some(25));
 
-        week_temperatures.set_temperature(Weekday::Tuesday, 30);
-        week_temperatures.set_temperature(Weekday::Wednesday, 35);
-        week_temperatures.set_temperature(Weekday::Thursday, 40);
-        week_temperatures.set_temperature(Weekday::Friday, 45);
-        week_temperatures.set_temperature(Weekday::Saturday, 50);
-        week_temperatures.set_temperature(Weekday::Sunday, 55);
+        week_temperatures.set_temperature(&Weekday::Tuesday, 30);
+        week_temperatures.set_temperature(&Weekday::Wednesday, 35);
+        week_temperatures.set_temperature(&Weekday::Thursday, 40);
+        week_temperatures.set_temperature(&Weekday::Friday, 45);
+        week_temperatures.set_temperature(&Weekday::Saturday, 50);
+        week_temperatures.set_temperature(&Weekday::Sunday, 55);
 
         assert_eq!(week_temperatures.get_temperature(Weekday::Monday), Some(25));
         assert_eq!(
