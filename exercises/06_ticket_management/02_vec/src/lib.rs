39658910fc63c97f272a5c0a81e6fc78abeceb65
ThisIsBasic <learningstill@proton.me>
2024-07-17 23:37:24 +0200

    ch 06 ex 02 solution - fibonacci

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,12 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut cache = vec![0, 1];
+    let n = n as usize;
+    for i in 2..=n {
+        cache.push(cache[i - 1] + cache[i - 2]);
+    }
+    cache[n]
 }
 
 #[cfg(test)]
